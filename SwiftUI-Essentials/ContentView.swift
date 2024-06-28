26c040dd1def1ae6d2fced5798049edc61ea6f0c
“PonceGL” <ponciano.guevara@digitalfemsa.com>
2024-06-28 16:10:14 -0600

    Initial Commit

---
+++ SwiftUI-Essentials/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Essentials
+//
+//  Created by Ponciano Guevara Lozano on 28/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Essentials/SwiftUI_EssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EssentialsApp.swift
+//  SwiftUI-Essentials
+//
+//  Created by Ponciano Guevara Lozano on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
